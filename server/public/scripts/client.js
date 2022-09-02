9af2e14a67dce7146135025fd72f92d66e7ae0b5
Kyle Jensen <kjensen19@gmail.com>
2022-09-01 23:33:22 -0500

    POST route running and receiving data object

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,59 @@ $(document).ready(handleReady);
 
 function handleReady() {
     console.log('JS/JQ')
+    $('#calc').on('click','button', clickCollector)
 }
+//create string variables to store button clicks
+let currentStr = ''
+let operandArr = ['-', '/', '+', '*', "C", "AC", '=']
+
+calcObj = []
+
+
+//function to concat strings, need to track operand to switch str, then equals
+function clickCollector() {
+    let inputVal = $(this).text()
+    console.log(`inputVal = ${inputVal}`)
+    if (operandArr.includes(inputVal)) {
+        if (inputVal === 'C' || inputVal === 'AC' || inputVal === '=') {
+            //handle C to clear the previous string, AC to reset or equals to send
+            console.log('A, AC, =')
+            if (inputVal === 'C') {
+                currentStr = ''
+            }
+            else if (inputVal === 'AC') {
+                currentStr = ''
+                calcObj.length(0)
+            }
+            else {
+                //call POST function
+                calcObj.push(currentStr)
+                sendExpression()
+            }
+        }
+        else {
+            //push string, push operand
+            calcObj.push(currentStr)
+            calcObj.push(inputVal)
+            console.log(`operand else ${calcObj}`)
+            currentStr = ''
+        }
+    }
+    else {
+        currentStr += inputVal
+        console.log(`currentStr = ${currentStr}`)
+    }
+    return currentStr
+}
+
+function sendExpression() {
+    $.ajax({
+        type: 'POST',
+        url: '/sendData',
+        data: {calcObj}
+      }).then(function(response) {
+        
+      })
+}
+    
+    
--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
-
+app.post('/sendData', (req, res) => {
+    console.log('hi', req.body)
+    res.sendStatus(201)
+})
 
 
 
